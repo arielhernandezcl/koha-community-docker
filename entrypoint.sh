42a8f53d483a83bf3f349a5089dfb4bc3510aa00
arielhernandezcl <ariel.dox@gmail.com>
2024-03-12 18:34:01 -0300

    update 3

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start Apache web server
-service apache2 start
-
-# Other initialization commands if needed
-
-# Keep the container running
-tail -f /dev/null
---
+++ run.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+
+ts() {
+    date -Ins
+}
+
+log() {
+    echo $( ts ) "$@"
+}
+logerr() {
+    echo $( ts ) "$@" > /dev/stderr
+}
+
+#if no koha instance name was provided, then set it as "default"
+export KOHA_INSTANCE=${KOHA_INSTANCE:-default}
+
+export KOHA_INTRANET_PORT=${KOHA_INTRANET_PORT:-8081}
+export KOHA_OPAC_PORT=${KOHA_OPAC_PORT:-8080}
+export USE_MEMCACHED=${USE_MEMCACHED:-yes}
+export MEMCACHED_SERVERS=${MEMCACHED_SERVERS:-memcached:11211}
+export MYSQL_SERVER=${MYSQL_SERVER:-db}
+export MYSQL_PASSWORD=${MYSQL_PASSWORD:-$(pwgen -s 15 1)}
+export ZEBRA_MARC_FORMAT=${ZEBRA_MARC_FORMAT:-marc21}
+export MYSQL_ROOT_USER=${MYSQL_ROOT_USER:-root}
+export KOHA_LIB_SHARE=${KOHA_LIB_SHARE:-/tmp/libshare}
+export KOHA_PLACK_NAME=${KOHA_PLACK_NAME:-koha}
+export KOHA_ES_NAME=${KOHA_ES_NAME:-es}
+
+log "Initialising koha instance $KOHA_INSTANCE"
+
+[ -d "$KOHA_LIB_SHARE" ] && [ ! "$(ls -A $KOHA_LIB_SHARE)" ] && cp -r /usr/share/koha/lib/* $KOHA_LIB_SHARE;
+
+envsubst < ./templates/koha-sites.conf > /etc/koha/koha-sites.conf
+echo -n "${KOHA_INSTANCE}:koha_${KOHA_INSTANCE}:${MYSQL_PASSWORD}:koha_${KOHA_INSTANCE}:${MYSQL_SERVER}" > /etc/koha/passwd
+
+if [ ! -f "/usr/share/koha/bin/koha-functions.sh" ]
+then
+    logerr "koha-functions should be present"
+    exit 1
+fi
+
+source /usr/share/koha/bin/koha-functions.sh
+
+if [ "${USE_BACKEND}" = "1" ] || [ "${USE_BACKEND}" = "true" ]
+then
+    #if there is no /var/lib/koha/<instance> directory, then we must install
+    if ! is_instance ${KOHA_INSTANCE} || [ ! -f "/etc/koha/sites/${KOHA_INSTANCE}/koha_conf.xml" ]
+    then
+        log "Creating instance ${KOHA_INSTANCE}"
+        #try to connect to database
+        mysql --user=${MYSQL_ROOT_USER} --password=${MYSQL_ROOT_PASSWORD} --host=${MYSQL_SERVER} koha_${KOHA_INSTANCE} -e "select 1" > /dev/null 2>&1
+
+        if [ "$?" != "0" ]
+        then
+            #Database or user not created
+            log "Creating database koha_${KOHA_INSTANCE}"
+            mysql --user=${MYSQL_ROOT_USER} --password=${MYSQL_ROOT_PASSWORD} --host=${MYSQL_SERVER} koha_${KOHA_INSTANCE} -e "CREATE DATABASE \`koha_${KOHA_INSTANCE}\`;"
+        fi
+
+        mysql --user=koha_${KOHA_INSTANCE} --password=${MYSQL_PASSWORD} --host=${MYSQL_SERVER} koha_${KOHA_INSTANCE} -e "select 1" > /dev/null 2>&1
+
+        if [ "$?" != "0" ]
+        then
+            log "Creating user koha_${KOHA_INSTANCE}"
+            mysql --user=${MYSQL_ROOT_USER} --password=${MYSQL_ROOT_PASSWORD} --host=${MYSQL_SERVER} mysql << EOF
+    CREATE USER \`koha_${KOHA_INSTANCE}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`koha_${KOHA_INSTANCE}\`.* TO \`koha_${KOHA_INSTANCE}\`@'%';
+    FLUSH PRIVILEGES;   
+EOF
+        fi
+    fi
+
+    if ! is_instance ${KOHA_INSTANCE} || [ ! -f "/etc/koha/sites/${KOHA_INSTANCE}/koha_conf.xml" ]
+    then
+        log "Executing koha-create for instance ${KOHA_INSTANCE}"
+        koha-create --use-db ${KOHA_INSTANCE} | true
+    else
+        log "Creating directories structure"
+        koha-create-dirs ${KOHA_INSTANCE}
+    fi
+
+    envsubst < ./templates/supervisor/plack.conf > /etc/supervisor/conf.d/plack.conf
+
+    if [ "${OVERRIDE_SYSPREF_SearchEngine}" != "Elasticsearch" ]
+    then
+        envsubst < ./templates/supervisor/zebra.conf > /etc/supervisor/conf.d/zebra.conf
+    else
+        log "Rebuilding elasticsearch indicies in the background"
+        koha-elasticsearch --rebuild -p $(grep -c ^processor /proc/cpuinfo) ${KOHA_INSTANCE} &
+    fi
+
+    for i in $(koha-translate -l)
+    do
+        if [ "${KOHA_LANGS}" = "" ] || ! echo "${KOHA_LANGS}"|grep -q -w $i
+        then
+            log "Removing language $i"
+            koha-translate -r $i
+        else
+            log "Checking language $i"
+            koha-translate -c $i
+        fi
+    done
+
+    if [ "${KOHA_LANGS}" != "" ]
+    then
+        log "Installing languages"
+        LANGS=$(koha-translate -l)
+        for i in $KOHA_LANGS
+        do
+            if ! echo "${LANGS}"|grep -q -w $i
+            then
+                log "Installing language $i"
+                koha-translate -i $i
+            else
+                log "Language $i already present"
+            fi
+        done
+    fi
+    touch /healthy
+fi
+
+if [ "$USE_SIP" = "1" ] || [ "$USE_SIP" = "true" ]
+then
+
+    echo "Configuring SIPServer"
+    SIP_CONF_ACCOUNTS=''
+    for ac in $SIP_ACCOUNTS
+    do
+        SIP_PWD='SIP_${ac}_PWD'
+        SIP_DLTR='SIP_${ac}_DLTR'
+        SIP_ERR='SIP_${ac}_ERR'
+        SIP_LIB='SIP_${ac}_LIB'
+        SIP_CONF_ACCOUNTS=$(cat << EOF
+        ${SIP_CONF_ACCOUNTS}
+        <login  id=\"${ac}\" password="${!SIP_PWD}"
+                delimiter="${!SIP_DLTR:-|}" error-detect="${!SIP_ERR:-enabled}" 
+                institution="${!SIP_LIB}"
+        />
+EOF
+)
+    done
+
+    SIP_CONF_LIBS=''
+    for lib in $SIP_LIBS
+    do
+        SIP_IMPL='SIP_${lib}_IMPL'
+        SIP_PARAMS='SIP_${lib}_PARAMS'
+        SIP_CI='SIP_${lib}_CI'
+        SIP_RNW='SIP_${lib}_RNW'
+        SIP_CO='SIP_${lib}_CO'
+        SIP_SU='SIP_${lib}_SU'
+        SIP_OL='SIP_${lib}_OL'
+        SIP_TO='SIP_${lib}_TO'
+        SIP_RET='SIP_${lib}_RET'
+        SIP_CONF_LIBS=$(cat << EOF
+        ${SIP_CONF_LIBS}
+        <institution id="${lib}" implementation="${!SIP_IMPL:-ILS}" parms="${!SIP_PARAMS}">
+         <policy checkin="${!SIP_CI:-true}" renewal="${!SIP_RNW:-true}" checkout="${!SIP_CO:-true}"
+                 status_update="${!SIP_SU:-false}" offline="${!SIP_OL:-false}"
+                 timeout="${!SIP_TO:-100}"
+                 retries="${!SIP_RET:-5}" />
+        </institution>
+EOF
+)
+    done
+
+    envsubst < ./templates/SIPconfig.xml > /etc/koha/sites/${KOHA_INSTANCE}/SIPconfig.xml
+    envsubst < ./templates/supervisor/sip.conf > /etc/supervisor/conf.d/sip.conf
+fi
+
+envsubst < ./templates/supervisor/supervisord.conf > /etc/supervisor/supervisord.conf
+
+if [ "${USE_APACHE2}" = "1" ] || [ "${USE_APACHE2}" = "true" ]
+then
+    log "Executing koha-create for instance ${KOHA_INSTANCE}"
+    koha-create --use-db ${KOHA_INSTANCE} | true
+
+    koha-plack --enable ${KOHA_INSTANCE}
+    envsubst < ./templates/supervisor/apache2.conf > /etc/supervisor/conf.d/apache2.conf
+
+    a2enmod proxy
+fi
+
+if [ "${USE_CRON}" = "1" ] || [ "${USE_CRON}" = "true" ]
+then
+    envsubst < ./templates/supervisor/cron.conf > /etc/supervisor/conf.d/cron.conf
+fi
+
+if [ "${USE_Z3950}" = "1" ] || [ "${USE_Z3950}" = "true" ]
+then
+    koha-z3950-responder --enable $KOHA_INSTANCE
+    envsubst < ./templates/supervisor/z3950.conf > /etc/supervisor/conf.d/z3950.conf
+fi
+
+service apache2 stop
+koha-indexer --stop ${KOHA_INSTANCE}
+koha-zebra --stop ${KOHA_INSTANCE}
+
+supervisord -c /etc/supervisor/supervisord.conf
